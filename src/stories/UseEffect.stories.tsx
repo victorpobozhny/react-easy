f077b29fdc91a98c5635855e17caaed9e1f587c0
Victor <pobozhny94@gmail.com>
2024-01-15 17:43:09 +0300

    lesson 22 useEffect/ Time homework.  Interval/Timeout

--- src/stories/UseEffect.stories.tsx
+++ src/stories/UseEffect.stories.tsx
@@ -12,7 +12,7 @@ export const Example1 = () => {
     useEffect(() => {
         console.log('useEffect when component update')
         document.title = state.toString()
-    }, )
+    },)
     useEffect(() => {
         console.log('useEffect only once')
         document.title = state.toString()
@@ -34,6 +34,62 @@ export const Example1 = () => {
         </div>
     );
 };
+export const SetTimeoutExample1 = () => {
+    console.log('Example1')
+
+    const [state, setState] = useState<number>(0)
+    const [fake, setFake] = useState(99)
+    // useEffect(()=>{
+    //     setTimeout(()=>{
+    //         document.title = state.toString()
+    //         console.log(`timeout. i'm changing title`)
+    //     }, 3000)
+    //
+    // }, [state])
+
+
+    useEffect(() => {
+        setInterval(() => {
+            setState(state => state + 1)
+        }, 1000)
+    }, [])
+
+    return (
+        <div>
+
+            Count
+            <button onClick={() => setState(state + 1)}>+</button>
+            <span>{state}</span>
+            <br/>
+            Fake
+            <button onClick={() => setFake(fake - 1)}>+</button>
+            <span>{fake}</span>
+        </div>
+    );
+};
+export const Clock = () => {
+    console.log('time update')
+    const current = new Date()
+
+    const [time, setTime] = useState([current.getHours(), current.getMinutes(), current.getSeconds()])
+    useEffect(() => {
+        setInterval(() => {
+            setTime([new Date().getHours(), new Date().getMinutes(), new Date().getSeconds()])
+        }, 1000)
+    }, [])
+
+
+    return (
+        <div>
+            <div>
+                <h3>Current Time</h3>
+                <h4>Minsk:</h4>
+                {`${time[0]}:${time[1]}:${time[2]}`}
+            </div>
+        </div>
+    );
+};
+
 
 
 
